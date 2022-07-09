2e78d7cd2a5142159a1db8c3437ce5a93ed7f239
Grid North Tech <akula802@users.noreply.github.com>
2022-07-08 20:30:07 -0600

    First commit

---
+++ basic-queries.sql
@@ -0,0 +1,5 @@
+USE testdb;
+#INSERT INTO countries (name, capitol, language) VALUES ('Germany', 'Berlin', 'German')
+#INSERT INTO countries (name, capitol, language) VALUES ('United States', 'Washington, DC', 'English');
+
+SELECT * FROM users
---
+++ database_connect.py
@@ -0,0 +1,85 @@
+import mysql.connector 
+
+
+# Connect to the server
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'def bobby_tables(INP):',
+    database = 'testdb',
+    #autocommit = 'true',
+    )
+
+
+# Create a cursor - first thing you need, pretty much always
+my_cursor = mydb.cursor(buffered=True)
+
+
+###############################
+###### Create a database ######
+###############################
+
+#my_cursor.execute("CREATE DATABASE testdb;")
+
+# Run a 'show databases' command - this just shows the command written out
+#my_cursor.execute("SHOW DATABASES")
+
+# For loop to actually print the databases
+#For db in my_cursor:
+#    print(db)
+
+
+############################
+###### Create a table ######
+############################
+
+# Create the table
+#my_cursor.execute("CREATE TABLE users (name VARCHAR(255), email VARCHAR(255), age INT(10), user_id INTEGER AUTO_INCREMENT PRIMARY key)")
+
+# Add some rows with data, one at a time
+#my_cursor.execute("INSERT INTO users (name, email, age) VALUES ('Muki', 'muki@gridnorth.tech', 4);")
+#my_cursor.execute("INSERT INTO users (name, email, age) VALUES ('Brian', 'brian@gridnorth.tech', 38);")
+
+# Show the table(s)
+#my_cursor.execute("SHOW TABLES")
+#for table in my_cursor:
+#    print(table)
+
+
+#############################################
+###### Create multiple records at once ######
+#############################################
+
+# Multiline comment sort of, per: https://www.w3schools.com/python/python_comments.asp
+"""
+sql_stuff = "INSERT INTO users (name, email, age) VALUES (%s, %s, %s)"
+records = [
+    ('Melissa', 'melissa@email.net', 36),
+    ('Lunchbox', 'lunchbox@catsemail.com', 8),
+    ('Rissypoo', 'sheis@psycho.path', 5),
+    ('theChicken', 'eatme@dinner.com', 3)
+]
+
+# Loop and write - don't need to loop if you use 'executemany'
+for record in records:
+    my_cursor.execute(sql_stuff, record)
+
+# Execute many
+my_cursor.executemany(sql_stuff, record)
+
+""" # End 'multiline comment'
+
+
+#####################################
+###### Commit changes and show ######
+#####################################
+
+# Commit the changes
+#mydb.commit()
+
+# Show the table data
+my_cursor.execute("SELECT * FROM users")
+for user in my_cursor:
+    print(user)
+
+
